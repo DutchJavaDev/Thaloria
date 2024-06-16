a6890e023c8fc829d0fd7d003493533c97f881a5
DutchJavaDev <borismulder@outlook.com>
2024-06-16 17:40:26 +0200

    base for #3

---
+++ Thaloria/World/ThaloriaGame.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thaloria.World
+{
+    public sealed class ThaloriaGame
+    {
+        public ThaloriaGame()
+        { }
+
+        // Main game loop
+        // Updating and rendering
+        public void Run()
+        {
+            
+        }
+    }
+}
