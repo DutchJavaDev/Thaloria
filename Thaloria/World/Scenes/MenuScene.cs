ce56127219db176751626281ac810dadc4dc0aca
DutchJavaDev <borismulder@outlook.com>
2024-06-19 00:19:36 +0200

    added play button

--- Thaloria/World/Scenes/MenuScene.cs
+++ Thaloria/World/Scenes/MenuScene.cs
@@ -1,45 +1,87 @@
 ﻿using Thaloria.World.Interface;
 using Raylib_cs;
 using static Raylib_cs.Raylib;
+using Thaloria.Loaders;
+using System.Numerics;
 
 namespace Thaloria.World.Scenes
 {
-  public sealed class MenuScene : IScene
-  {
-    public SceneManagerEnum SceneReference => SceneManagerEnum.MenuScene;
-    private SceneManager? _sceneManager;
-
-    private int x = 150;
-    private int y = 150;
-    private int force = 75;
-
-    public void Init(SceneManager sceneManager)
+    public sealed class MenuScene : IScene
     {
-      _sceneManager = sceneManager;
-    }
-    public async Task Load()
-    {
-      await Task.Delay(1000);
-    }
+        public SceneManagerEnum SceneReference => SceneManagerEnum.MenuScene;
+        private SceneManager? _sceneManager;
 
-    public Task Dispose()
-    {
-      return Task.CompletedTask;
-    }
+        private readonly string _playText = "..Play..";
+        private const int _fontSize = 45;
+        private const int _spacing = 5;
+        private Font _imortalFont;
+        private Color _playColor = Color.Red;
 
-    public void Update()
-    {
-      if (IsKeyPressed(KeyboardKey.Enter))
-      {
-        _sceneManager?.SwitchToScene(SceneManagerEnum.GameScene);
-      }
-    }
+        private Rectangle _playRectangle;
+        private Vector2 _playTextPosition;
 
-    public void Render()
-    {
-      BeginDrawing();
-      DrawText("Menu loaded", x, y, 18, Color.Green);
-      EndDrawing();
+        public void Init(SceneManager sceneManager)
+        {
+            _sceneManager = sceneManager;
+        }
+
+        public Task Load()
+        {
+            _imortalFont = FontManager.GetFont(0);
+            var screenWidth = (float)GetScreenWidth();
+            var screenHeight = (float)GetScreenHeight();
+            
+            var fontSize = MeasureTextEx(_imortalFont, _playText, _fontSize, _spacing);
+            // Place text in the center of that rectangle, have to do this instead, normal way aint working
+            var buttonWidth = fontSize.X;
+            var buttonHeight = fontSize.Y;
+
+            // Place rectangle at center of screen
+            var position = new Vector2(screenWidth / 2-(buttonWidth / 2),screenHeight/2-(buttonHeight / 2));
+            _playRectangle = new() 
+            {
+                Position = position,
+                Width = buttonWidth,
+                Height = buttonHeight
+            };
+
+            // Place text in the center of that rectangle
+            var x = _playRectangle.X;
+            var y = _playRectangle.Y;
+            _playTextPosition = new Vector2(x, y);
+            return Task.CompletedTask;
+        }
+
+        public Task Dispose()
+        {
+            return Task.CompletedTask;
+        }
+
+        public void Update()
+        {
+            var mouse = GetMousePosition();
+
+            if (CheckCollisionPointRec(mouse, _playRectangle))
+            {
+                _playColor = Color.Green;
+            }
+            else
+            {
+                _playColor = Color.Red;
+            }
+
+            if (IsMouseButtonPressed(MouseButton.Left) && CheckCollisionPointRec(mouse,_playRectangle))
+            {
+                _sceneManager?.SwitchToScene(SceneManagerEnum.GameScene);
+            }
+        }
+
+        public void Render()
+        {
+            BeginDrawing();
+            DrawRectangleLinesEx(_playRectangle,2,Color.LightGray);
+            DrawTextEx(_imortalFont, _playText, _playTextPosition, _fontSize, _spacing, _playColor);
+            EndDrawing();
+        }
     }
-  }
 }
