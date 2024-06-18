a69f7274403f929335fc507a74af1f93d610ac49
DutchJavaDev <borismulder@outlook.com>
2024-06-19 00:19:07 +0200

    vs

--- Thaloria/World/Scenes/DefaultScene.cs
+++ Thaloria/World/Scenes/DefaultScene.cs
@@ -7,85 +7,85 @@ using Thaloria.Loaders;
 namespace Thaloria.World.Scenes
 {
 
-  /// <summary>
-  /// Loading scene
-  /// </summary>
-  public sealed class DefaultScene : IScene
-  {
-    public SceneManagerEnum SceneReference => SceneManagerEnum.DefaultScene;
-    private SceneManager? _sceneManager;
-
-    private const string _loadingText = "...Loading...";
-    private const int _fontSize = 45;
-    private const int _spacing = 5;
-    private Font _imortalFont;
-    private Vector2 _loadTextPosition;
-    private Color _loadColor;
-    private float _loadColorAlpha = 1f;
-    private int _ticks = 0;
-    private readonly int _updateTickRate = 6;
-    private float _direction = -0.2f;
-
-    public void Init(SceneManager sceneManager) 
+    /// <summary>
+    /// Loading scene
+    /// </summary>
+    public sealed class DefaultScene : IScene
     {
-      _sceneManager = sceneManager;
-      _imortalFont = FontManager.GetFont(0);
-      var fontSize = MeasureTextEx(_imortalFont, _loadingText, _fontSize, _spacing);
-      var width = GetScreenWidth();
-      var height = GetScreenHeight();
-      var x = (width / 2) - fontSize.X / 2;
-      var y = (height / 2) - fontSize.Y / 2;
-      _loadTextPosition = new Vector2(x, y);
-      _loadColor = Color.LightGray;
-    }
+        public SceneManagerEnum SceneReference => SceneManagerEnum.DefaultScene;
+        private SceneManager? _sceneManager;
 
-    public Task Dispose()
-    {
-      return Task.CompletedTask;
-    }
+        private const string _loadingText = "...Loading...";
+        private const int _fontSize = 45;
+        private const int _spacing = 5;
+        private Font _imortalFont;
+        private Vector2 _loadTextPosition;
+        private Color _loadColor;
+        private float _loadColorAlpha = 1f;
+        private int _ticks = 0;
+        private readonly int _updateTickRate = 6;
+        private float _direction = -0.2f;
 
-    public Task Load()
-    {
-      return Task.CompletedTask;
-    }
-
-    public void Update()
-    {
-      UpdateAlpha();
-    }
-
-    private void UpdateAlpha()
-    {
-      _ticks++;
-
-      if (_ticks == _updateTickRate)
-      {
-        _loadColorAlpha += _direction;
-
-        if (_loadColorAlpha < 0.3f)
+        public void Init(SceneManager sceneManager)
         {
-          _direction = 0.2f;
-          _loadColorAlpha = 0.3f;
+            _sceneManager = sceneManager;
+            _imortalFont = FontManager.GetFont(0);
+            var fontSize = MeasureTextEx(_imortalFont, _loadingText, _fontSize, _spacing);
+            var width = GetScreenWidth();
+            var height = GetScreenHeight();
+            var x = (width / 2) - fontSize.X / 2;
+            var y = (height / 2) - fontSize.Y / 2;
+            _loadTextPosition = new Vector2(x, y);
+            _loadColor = Color.LightGray;
         }
 
-        if (_loadColorAlpha > 1f)
+        public Task Dispose()
         {
-          _direction = -0.2f;
-          _loadColorAlpha = 1f;
+            return Task.CompletedTask;
         }
 
-        _ticks = 0;
+        public Task Load()
+        {
+            return Task.CompletedTask;
+        }
 
-        _loadColor = ColorAlpha(_loadColor, _loadColorAlpha);
-      }
+        public void Update()
+        {
+            UpdateAlpha();
+        }
 
+        private void UpdateAlpha()
+        {
+            _ticks++;
+
+            if (_ticks == _updateTickRate)
+            {
+                _loadColorAlpha += _direction;
+
+                if (_loadColorAlpha < 0.3f)
+                {
+                    _direction = 0.2f;
+                    _loadColorAlpha = 0.3f;
+                }
+
+                if (_loadColorAlpha > 1f)
+                {
+                    _direction = -0.2f;
+                    _loadColorAlpha = 1f;
+                }
+
+                _ticks = 0;
+
+                _loadColor = ColorAlpha(_loadColor, _loadColorAlpha);
+            }
+
+        }
+
+        public void Render()
+        {
+            BeginDrawing();
+            DrawTextEx(_imortalFont, _loadingText, _loadTextPosition, _fontSize, _spacing, _loadColor);
+            EndDrawing();
+        }
     }
-
-    public void Render()
-    {
-      BeginDrawing();
-      DrawTextEx(_imortalFont,_loadingText,_loadTextPosition,_fontSize,_spacing,_loadColor);
-      EndDrawing();
-    }
-  }
 }
