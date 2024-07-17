f473412eae3ce9f376972abc41a08d9f1f57a5e3
DutchJavaDev <borismulder@outlook.com>
2024-07-17 16:44:12 +0200

    Create InputSystem.cs

---
+++ Thaloria/Game/ECS/Systems/InputSystem.cs
@@ -0,0 +1,42 @@
+﻿using DefaultEcs.System;
+using Raylib_cs;
+using static Raylib_cs.Raylib;
+using Thaloria.Game.ECS.Components;
+using DefaultEcs;
+
+namespace Thaloria.Game.ECS.Systems
+{
+  [With(typeof(PlayerComponent))]
+  public sealed class InputSystem(World world) : AEntitySetSystem<float>(world)
+  {
+    private readonly float _speed = 75f;
+
+    protected override void Update(float state, in Entity entity)
+    {
+      ref PlayerComponent player = ref entity.Get<PlayerComponent>();
+
+      var velocity = _speed * state;
+
+      if (IsKeyDown(KeyboardKey.A))
+      {
+        player.Body.X -= velocity;
+      }
+
+      if (IsKeyDown(KeyboardKey.D))
+      {
+        player.Body.X += velocity;
+      }
+
+      if (IsKeyDown(KeyboardKey.W))
+      {
+        player.Body.Y -= velocity;
+      }
+
+      if (IsKeyDown(KeyboardKey.S))
+      {
+        player.Body.Y += velocity;
+      }
+    }
+
+  }
+}
