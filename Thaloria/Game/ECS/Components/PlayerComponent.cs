6696b0090fb315dd0ad9e025ee809022fcbab20f
DutchJavaDev <borismulder@outlook.com>
2024-07-17 16:44:02 +0200

    Create PlayerComponent.cs

---
+++ Thaloria/Game/ECS/Components/PlayerComponent.cs
@@ -0,0 +1,11 @@
+﻿using Raylib_cs;
+
+namespace Thaloria.Game.ECS.Components
+{
+  public struct PlayerComponent(Rectangle initialBody)
+  {
+    public Rectangle Body = initialBody;
+
+    public readonly Color Color = Color.White;
+  }
+}
