a62a7fd5cdf4ce68ed71f29fb4359b82fb8dd10a
DutchJavaDev <borismulder@outlook.com>
2024-07-17 16:43:58 +0200

    Create CameraComponent.cs

---
+++ Thaloria/Game/ECS/Components/CameraComponent.cs
@@ -0,0 +1,10 @@
+﻿using Raylib_cs;
+
+namespace Thaloria.Game.ECS.Components
+{
+  public struct CameraComponent(Camera2D initialCamera)
+  {
+    public Camera2D Camera2D = initialCamera;
+    public Rectangle CameraView = new();
+  }
+}
