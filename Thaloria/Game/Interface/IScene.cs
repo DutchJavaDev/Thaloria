d0ac4c88cd21f090ac06fb415ad9d33597e122bf
DutchJavaDev <borismulder@outlook.com>
2024-07-30 22:07:35 +0200

    Update IScene.cs

--- Thaloria/Game/Interface/IScene.cs
+++ Thaloria/Game/Interface/IScene.cs
@@ -4,9 +4,9 @@
   {
     SceneManagerEnum SceneReference { get; }
     void Init(SceneManager sceneManager);
-    Task Load();
+    Task LoadAsync();
     void Update();
     void Render();
-    Task Dispose();
+    Task DisposeAsync();
   }
 }
