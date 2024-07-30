a49deaef3bff532d70ae3ec5a112cc2619ba593e
DutchJavaDev <borismulder@outlook.com>
2024-07-30 22:09:00 +0200

    Create PositionComponent.cs

---
+++ Thaloria/Game/ECS/Components/PositionComponent.cs
@@ -0,0 +1,11 @@
+﻿using System.Numerics;
+
+namespace Thaloria.Game.ECS.Components
+{
+  public struct PositionComponent
+  {
+    public Vector2 Position { get; set; }
+    public readonly float X => Position.X;
+    public readonly float Y => Position.Y;
+  }
+}
