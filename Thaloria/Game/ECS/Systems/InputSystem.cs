d0cd1d3011ad729826ee5e2836a0ae8b5834f507
DutchJavaDev <borismulder@outlook.com>
2024-09-13 18:31:07 +0200

    Update InputSystem.cs

--- Thaloria/Game/ECS/Systems/InputSystem.cs
+++ Thaloria/Game/ECS/Systems/InputSystem.cs
@@ -3,50 +3,61 @@ using Raylib_cs;
 using static Raylib_cs.Raylib;
 using Thaloria.Game.ECS.Components;
 using DefaultEcs;
+using Thaloria.Game.Physics;
+using Thaloria.Game.Helpers;
+using nkast.Aether.Physics2D.Common;
 
 namespace Thaloria.Game.ECS.Systems
 {
   [With(typeof(PlayerComponent))]
-  public sealed class InputSystem(World world) : AEntitySetSystem<float>(world)
+  public sealed class InputSystem() : AEntitySetSystem<float>(EcsCreation.Instance)
   {
-    private readonly float _speed = 75f;
+    private readonly float _speed = 100f;
 
     protected override void Update(float state, in Entity entity)
     {
-      ref PositionComponent playerPosition = ref entity.Get<PositionComponent>();
-
-      playerPosition.XVelocity = 0;
-      playerPosition.YVelocity = 0;
-
+      var body = PhysicsWorld.Instance.GetBodyByTag(entity.GetHashCode());
+      ref var animationController = ref entity.Get<AnimationComponent>();
       var velocity = _speed * state;
-      var xPosition = playerPosition.X;
-      var yPosition = playerPosition.Y;
+
+      // Reset to stop moving
+      body.LinearVelocity = Vector2.Zero;
+      //animationController.SetAnimation(AnimationTypes.Idle);
+      
+      // DEV
+      //if (IsKeyDown(KeyboardKey.L))
+      //{
+      //  body.FixedRotation = !body.FixedRotation;
+
+      //  if (body.FixedRotation) 
+      //  {
+      //    body.Rotation = 0;
+      //  }
+      //}
 
       if (IsKeyDown(KeyboardKey.A))
       {
-        xPosition -= velocity;
-        playerPosition.XVelocity = velocity * -1;
+        body.LinearVelocity += new Vector2(velocity * -1, 0);
+        animationController.SetAnimation(AnimationTypes.Walking_Left);
       }
 
       if (IsKeyDown(KeyboardKey.D))
       {
-        xPosition += velocity;
-        playerPosition.XVelocity = velocity;
+        body.LinearVelocity += new Vector2(velocity, 0);
+        animationController.SetAnimation(AnimationTypes.Walking_Right);
       }
 
       if (IsKeyDown(KeyboardKey.W))
       {
-        yPosition -= velocity;
-        playerPosition.YVelocity = velocity * -1;
+        body.LinearVelocity += new Vector2(0, velocity * -1);
+        animationController.SetAnimation(AnimationTypes.Walking_Up);
       }
 
       if (IsKeyDown(KeyboardKey.S))
       {
-        yPosition += velocity;
-        playerPosition.YVelocity = velocity;
+        body.LinearVelocity += new Vector2(0, velocity);
+        animationController.SetAnimation(AnimationTypes.Walking_Down);
       }
-
-      playerPosition.Position = new(xPosition,yPosition);
     }
 
   }
