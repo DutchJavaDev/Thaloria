ab510a230bf59ac87c0ca904a095a6fcfcb5bdf0
DutchJavaDev <borismulder@outlook.com>
2024-09-13 18:30:43 +0200

    Update RenderComponent.cs

--- Thaloria/Game/ECS/Components/RenderComponent.cs
+++ Thaloria/Game/ECS/Components/RenderComponent.cs
@@ -5,7 +5,7 @@ namespace Thaloria.Game.ECS.Components
   public struct RenderComponent
   {
     /// <summary>
-    /// Determine if this component has a texture or not, if not it will render a shape?
+    /// Determine if this component has a texture or not, if not it will render a shape
     /// </summary>
     public readonly bool HasTexture { get; init; }
 
@@ -13,5 +13,11 @@ namespace Thaloria.Game.ECS.Components
     ///  Color for the shape
     /// </summary>
     public Color RenderColor { get; set; }
+
+    public string TextureName { get; init; }
+
+    public int TextureWidth { get; init; }
+
+    public int TextureHeight { get; init; }
   }
 }
