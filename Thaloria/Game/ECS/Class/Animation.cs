655d49fce87bcebe01f6a782115a563f92eb3b34
DutchJavaDev <borismulder@outlook.com>
2024-09-14 20:52:43 +0200

    Create Animation.cs

---
+++ Thaloria/Game/ECS/Class/Animation.cs
@@ -0,0 +1,12 @@
+﻿using Thaloria.Game.Helpers;
+
+namespace Thaloria.Game.ECS.Class
+{
+  public sealed class Animation(AnimationTypes animationName, int frameCount, int rowStart, bool flipTexture = false)
+  {
+    public readonly AnimationTypes AnimationName = animationName;
+    public readonly int FrameCount = frameCount;
+    public readonly int RowStart = rowStart;
+    public readonly bool FlipTexture = flipTexture;
+  }
+}
