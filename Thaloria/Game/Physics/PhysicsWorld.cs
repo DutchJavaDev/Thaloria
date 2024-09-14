26783948e4bbd3f204ab506609ec85ecdbe51219
DutchJavaDev <borismulder@outlook.com>
2024-09-14 20:54:24 +0200

    Create PhysicsWorld.cs

---
+++ Thaloria/Game/Physics/PhysicsWorld.cs
@@ -0,0 +1,58 @@
+﻿using nkast.Aether.Physics2D.Dynamics;
+using nkast.Aether.Physics2D.Common;
+using DefaultEcs.System;
+
+namespace Thaloria.Game.Physics
+{
+  public sealed class PhysicsWorld : ISystem<float>
+  {
+    public static PhysicsWorld Instance;
+    private readonly World World;
+
+    static PhysicsWorld() 
+    {
+      Instance = new PhysicsWorld();
+    }
+
+    PhysicsWorld() 
+    {
+      World = new World(new Vector2(0,0));
+    }
+
+    public bool IsEnabled { get; set; }
+
+    public void CreateDynamicBody(float x, float y, float width, float height, int tag)
+    {
+      var body = World.CreateRectangle(width, height, 1f, new Vector2(x, y), 0, BodyType.Dynamic);
+      body.FixedRotation = true;
+      body.Tag = tag;
+    }
+
+    public void CreateStaticBody(float x, float y, float width, float height)
+    {
+      var body = World.CreateRectangle(width, height, 1f, new Vector2(x, y),0);
+      body.Tag = -1;
+    }
+
+    public Body GetBodyByTag(int tag)
+    {
+      return World.BodyList.First(i => i.Tag.Equals(tag));
+    }
+
+    public BodyCollection GetBodies() => World.BodyList;
+
+    public void Update(float state)
+    {
+      // Not all objects need to awake
+      // Check using the camera en set awake to false or true
+
+      var stepTime = 1f / state;
+      World.Step(stepTime);
+    }
+
+    public void Dispose()
+    {
+      World.Clear();
+    }
+  }
+}
