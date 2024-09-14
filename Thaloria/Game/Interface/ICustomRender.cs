0d75570bd1eaaa7c943666dd530624ae19c886f7
DutchJavaDev <borismulder@outlook.com>
2024-09-14 20:54:12 +0200

    Create ICustomRender.cs

---
+++ Thaloria/Game/Interface/ICustomRender.cs
@@ -0,0 +1,11 @@
+﻿using DefaultEcs.System;
+
+namespace Thaloria.Game.Interface
+{
+  public interface ICustomRender : ISystem<float>
+  {
+    void PreRender(float state);
+    void Render(float state);
+    void PostRender(float state);
+  }
+}
