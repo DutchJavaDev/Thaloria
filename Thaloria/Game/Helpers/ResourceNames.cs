2271a7b36f011eb4a09177bbebf6d0bafcf5799b
DutchJavaDev <borismulder@outlook.com>
2024-09-14 20:54:07 +0200

    Create ResourceNames.cs

---
+++ Thaloria/Game/Helpers/ResourceNames.cs
@@ -0,0 +1,9 @@
+﻿namespace Thaloria.Game.Helpers
+{
+  public static class ResourceNames
+  {
+    public static readonly string ImmortalFont = "immortal_font";
+    public static readonly string TileTexture = "map_tile_set";
+    public static readonly string PlayerTileset = "player_tile_set";
+  }
+}
