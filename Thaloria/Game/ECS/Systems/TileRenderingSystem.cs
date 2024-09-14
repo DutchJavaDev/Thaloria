9fabc3685f1b40444ae7772b810dfd1b1a1787c8
DutchJavaDev <borismulder@outlook.com>
2024-09-14 20:53:58 +0200

    Create TileRenderingSystem.cs

---
+++ Thaloria/Game/ECS/Systems/TileRenderingSystem.cs
@@ -0,0 +1,41 @@
+﻿using DefaultEcs;
+using DefaultEcs.System;
+using Raylib_cs;
+using Thaloria.Game.ECS.Components;
+using Thaloria.Game.Helpers;
+using Thaloria.Game.Map;
+using Thaloria.Loaders;
+using static Raylib_cs.Raylib;
+
+namespace Thaloria.Game.ECS.Systems
+{
+  public sealed class TileRenderingSystem(World world, TileData[] tiles, MapLoader Map) : ISystem<float>
+  {
+    public bool IsEnabled { get; set; }
+    private readonly TileData[] Tiles = tiles;
+    private readonly Texture2D TileTexture = ResourceManager.GetTexture2DTileset(ResourceNames.TileTexture);
+
+    public void Dispose()
+    {}
+
+    public void Update(float state)
+    {
+      ref CameraComponent cameraComponent = ref world.Get<CameraComponent>();
+
+      BeginMode2D(cameraComponent.Camera2D);
+      for (int i = 0; i < Tiles.Length; i++)
+      {
+        ref TileData tile =  ref Tiles[i];
+
+        var x = tile.RenderPosition.X;
+        var y = tile.RenderPosition.Y;
+
+        if (CheckCollisionRecs(cameraComponent.CameraView, new Rectangle(x, y, Map.TileWidth, Map.TileHeight)))
+        {
+          DrawTextureRec(TileTexture, tile.TexturePosition, tile.RenderPosition, Color.White);
+        }
+      }
+      EndMode2D();
+    }
+  }
+}
