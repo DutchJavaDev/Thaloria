b4c24a3150c6f0f7d65e6ef598db3ffa2fda6e2c
DutchJavaDev <borismulder@outlook.com>
2024-09-22 23:21:50 +0200

    mark source as loaded + check if tile is part of source

--- Thaloria/Game/Map/Tiled/TiledMapTileSet.cs
+++ Thaloria/Game/Map/Tiled/TiledMapTileSet.cs
@@ -4,10 +4,13 @@ namespace Thaloria.Game.Map.Tiled
 {
   public sealed class TiledMapTileSet
   {
-    //[JsonPropertyName("firstgid")]
-    //public int Firstgid { get; set; }
+    [JsonPropertyName("firstgid")]
+    public int Firstgid { get; set; }
 
     [JsonPropertyName("source")]
     public string Source { get; set; }
+
+    [JsonIgnore]
+    public bool Loaded { get; set; }
   }
 }
