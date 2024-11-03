268c088ce534cc16aa580c59211bc18abda327b0
DutchJavaDev <borismulder@outlook.com>
2024-11-03 22:55:15 +0100

    Update game resources

---
+++ Thaloria/Resources/Tiled/enviroment.tsx
@@ -0,0 +1,545 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="enviroment" tilewidth="16" tileheight="16" tilecount="1024" columns="32">
+ <editorsettings>
+  <export target="../Maps/enviroment.tsj" format="json"/>
+ </editorsettings>
+ <image source="../Tilesets/enviroment.png" width="512" height="512"/>
+ <tile id="18" type="tree_normal_height"/>
+ <tile id="19">
+  <properties>
+   <property name="parent_id" type="int" value="18"/>
+  </properties>
+ </tile>
+ <tile id="20">
+  <properties>
+   <property name="parent_id" type="int" value="18"/>
+  </properties>
+ </tile>
+ <tile id="21" type="tree_flowers"/>
+ <tile id="22">
+  <properties>
+   <property name="parent_id" type="int" value="21"/>
+  </properties>
+ </tile>
+ <tile id="23">
+  <properties>
+   <property name="parent_id" type="int" value="21"/>
+  </properties>
+ </tile>
+ <tile id="50">
+  <properties>
+   <property name="parent_id" type="int" value="18"/>
+  </properties>
+ </tile>
+ <tile id="51">
+  <properties>
+   <property name="parent_id" type="int" value="18"/>
+  </properties>
+ </tile>
+ <tile id="52">
+  <properties>
+   <property name="parent_id" type="int" value="18"/>
+  </properties>
+ </tile>
+ <tile id="53">
+  <properties>
+   <property name="parent_id" type="int" value="21"/>
+  </properties>
+ </tile>
+ <tile id="54">
+  <properties>
+   <property name="parent_id" type="int" value="21"/>
+  </properties>
+ </tile>
+ <tile id="55">
+  <properties>
+   <property name="parent_id" type="int" value="21"/>
+  </properties>
+ </tile>
+ <tile id="82">
+  <properties>
+   <property name="parent_id" type="int" value="18"/>
+  </properties>
+ </tile>
+ <tile id="83">
+  <properties>
+   <property name="parent_id" type="int" value="18"/>
+  </properties>
+ </tile>
+ <tile id="84">
+  <properties>
+   <property name="parent_id" type="int" value="18"/>
+  </properties>
+ </tile>
+ <tile id="85">
+  <properties>
+   <property name="parent_id" type="int" value="21"/>
+  </properties>
+ </tile>
+ <tile id="86">
+  <properties>
+   <property name="parent_id" type="int" value="21"/>
+  </properties>
+ </tile>
+ <tile id="87">
+  <properties>
+   <property name="parent_id" type="int" value="21"/>
+  </properties>
+ </tile>
+ <tile id="114">
+  <properties>
+   <property name="parent_id" type="int" value="18"/>
+  </properties>
+ </tile>
+ <tile id="115">
+  <properties>
+   <property name="parent_id" type="int" value="18"/>
+  </properties>
+ </tile>
+ <tile id="116">
+  <properties>
+   <property name="parent_id" type="int" value="18"/>
+  </properties>
+ </tile>
+ <tile id="117">
+  <properties>
+   <property name="parent_id" type="int" value="21"/>
+  </properties>
+ </tile>
+ <tile id="118">
+  <properties>
+   <property name="parent_id" type="int" value="21"/>
+  </properties>
+ </tile>
+ <tile id="119">
+  <properties>
+   <property name="parent_id" type="int" value="21"/>
+  </properties>
+ </tile>
+ <tile id="256" type="tree_flowers_height"/>
+ <tile id="257">
+  <properties>
+   <property name="parent_id" type="int" value="256"/>
+  </properties>
+ </tile>
+ <tile id="258">
+  <properties>
+   <property name="parent_id" type="int" value="256"/>
+  </properties>
+ </tile>
+ <tile id="259" type="tree_normal"/>
+ <tile id="260">
+  <properties>
+   <property name="parent_id" type="int" value="259"/>
+  </properties>
+ </tile>
+ <tile id="261">
+  <properties>
+   <property name="parent_id" type="int" value="259"/>
+  </properties>
+ </tile>
+ <tile id="262" type="grass_plain_obstacle"/>
+ <tile id="263">
+  <properties>
+   <property name="parent_id" type="int" value="262"/>
+  </properties>
+ </tile>
+ <tile id="264">
+  <properties>
+   <property name="parent_id" type="int" value="262"/>
+  </properties>
+ </tile>
+ <tile id="265" type="grass_plain_obstacle_vertical"/>
+ <tile id="288">
+  <properties>
+   <property name="parent_id" type="int" value="256"/>
+  </properties>
+ </tile>
+ <tile id="289">
+  <properties>
+   <property name="parent_id" type="int" value="256"/>
+  </properties>
+ </tile>
+ <tile id="290">
+  <properties>
+   <property name="parent_id" type="int" value="256"/>
+  </properties>
+ </tile>
+ <tile id="291">
+  <properties>
+   <property name="parent_id" type="int" value="259"/>
+  </properties>
+ </tile>
+ <tile id="292">
+  <properties>
+   <property name="parent_id" type="int" value="259"/>
+  </properties>
+ </tile>
+ <tile id="293">
+  <properties>
+   <property name="parent_id" type="int" value="259"/>
+  </properties>
+ </tile>
+ <tile id="294">
+  <properties>
+   <property name="parent_id" type="int" value="262"/>
+  </properties>
+ </tile>
+ <tile id="295">
+  <properties>
+   <property name="parent_id" type="int" value="262"/>
+  </properties>
+ </tile>
+ <tile id="296">
+  <properties>
+   <property name="parent_id" type="int" value="262"/>
+  </properties>
+ </tile>
+ <tile id="297">
+  <properties>
+   <property name="parent_id" type="int" value="265"/>
+  </properties>
+ </tile>
+ <tile id="320">
+  <properties>
+   <property name="parent_id" type="int" value="256"/>
+  </properties>
+ </tile>
+ <tile id="321">
+  <properties>
+   <property name="parent_id" type="int" value="256"/>
+  </properties>
+ </tile>
+ <tile id="322">
+  <properties>
+   <property name="parent_id" type="int" value="256"/>
+  </properties>
+ </tile>
+ <tile id="323">
+  <properties>
+   <property name="parent_id" type="int" value="259"/>
+  </properties>
+ </tile>
+ <tile id="324">
+  <properties>
+   <property name="parent_id" type="int" value="259"/>
+  </properties>
+ </tile>
+ <tile id="325">
+  <properties>
+   <property name="parent_id" type="int" value="259"/>
+  </properties>
+ </tile>
+ <tile id="326">
+  <properties>
+   <property name="parent_id" type="int" value="262"/>
+  </properties>
+ </tile>
+ <tile id="327">
+  <properties>
+   <property name="parent_id" type="int" value="262"/>
+  </properties>
+ </tile>
+ <tile id="328">
+  <properties>
+   <property name="parent_id" type="int" value="262"/>
+  </properties>
+ </tile>
+ <tile id="329">
+  <properties>
+   <property name="parent_id" type="int" value="265"/>
+  </properties>
+ </tile>
+ <tile id="352">
+  <properties>
+   <property name="parent_id" type="int" value="256"/>
+  </properties>
+ </tile>
+ <tile id="353">
+  <properties>
+   <property name="parent_id" type="int" value="256"/>
+  </properties>
+ </tile>
+ <tile id="354">
+  <properties>
+   <property name="parent_id" type="int" value="256"/>
+  </properties>
+ </tile>
+ <tile id="355">
+  <properties>
+   <property name="parent_id" type="int" value="259"/>
+  </properties>
+ </tile>
+ <tile id="356">
+  <properties>
+   <property name="parent_id" type="int" value="259"/>
+  </properties>
+ </tile>
+ <tile id="357">
+  <properties>
+   <property name="parent_id" type="int" value="259"/>
+  </properties>
+ </tile>
+ <tile id="384">
+  <animation>
+   <frame tileid="384" duration="100"/>
+   <frame tileid="389" duration="100"/>
+   <frame tileid="394" duration="100"/>
+   <frame tileid="399" duration="100"/>
+   <frame tileid="404" duration="100"/>
+   <frame tileid="409" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="385">
+  <animation>
+   <frame tileid="385" duration="100"/>
+   <frame tileid="390" duration="100"/>
+   <frame tileid="395" duration="100"/>
+   <frame tileid="400" duration="100"/>
+   <frame tileid="405" duration="100"/>
+   <frame tileid="410" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="386">
+  <animation>
+   <frame tileid="386" duration="100"/>
+   <frame tileid="391" duration="100"/>
+   <frame tileid="396" duration="100"/>
+   <frame tileid="401" duration="100"/>
+   <frame tileid="406" duration="100"/>
+   <frame tileid="411" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="387">
+  <animation>
+   <frame tileid="387" duration="100"/>
+   <frame tileid="392" duration="100"/>
+   <frame tileid="397" duration="100"/>
+   <frame tileid="402" duration="100"/>
+   <frame tileid="407" duration="100"/>
+   <frame tileid="412" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="388">
+  <animation>
+   <frame tileid="388" duration="100"/>
+   <frame tileid="393" duration="100"/>
+   <frame tileid="398" duration="100"/>
+   <frame tileid="403" duration="100"/>
+   <frame tileid="408" duration="100"/>
+   <frame tileid="413" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="416">
+  <animation>
+   <frame tileid="416" duration="100"/>
+   <frame tileid="421" duration="100"/>
+   <frame tileid="426" duration="100"/>
+   <frame tileid="431" duration="100"/>
+   <frame tileid="436" duration="100"/>
+   <frame tileid="441" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="418">
+  <animation>
+   <frame tileid="418" duration="100"/>
+   <frame tileid="423" duration="100"/>
+   <frame tileid="428" duration="100"/>
+   <frame tileid="433" duration="100"/>
+   <frame tileid="438" duration="100"/>
+   <frame tileid="443" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="419">
+  <animation>
+   <frame tileid="419" duration="100"/>
+   <frame tileid="424" duration="100"/>
+   <frame tileid="429" duration="100"/>
+   <frame tileid="434" duration="100"/>
+   <frame tileid="439" duration="100"/>
+   <frame tileid="444" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="420">
+  <animation>
+   <frame tileid="420" duration="100"/>
+   <frame tileid="425" duration="100"/>
+   <frame tileid="430" duration="100"/>
+   <frame tileid="435" duration="100"/>
+   <frame tileid="440" duration="100"/>
+   <frame tileid="445" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="448">
+  <animation>
+   <frame tileid="448" duration="100"/>
+   <frame tileid="453" duration="100"/>
+   <frame tileid="458" duration="100"/>
+   <frame tileid="463" duration="100"/>
+   <frame tileid="468" duration="100"/>
+   <frame tileid="473" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="449">
+  <animation>
+   <frame tileid="449" duration="100"/>
+   <frame tileid="454" duration="100"/>
+   <frame tileid="459" duration="100"/>
+   <frame tileid="464" duration="100"/>
+   <frame tileid="469" duration="100"/>
+   <frame tileid="474" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="450">
+  <animation>
+   <frame tileid="450" duration="100"/>
+   <frame tileid="455" duration="100"/>
+   <frame tileid="460" duration="100"/>
+   <frame tileid="465" duration="100"/>
+   <frame tileid="470" duration="100"/>
+   <frame tileid="475" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="485" type="bush_height"/>
+ <tile id="486">
+  <properties>
+   <property name="parent_id" type="int" value="485"/>
+  </properties>
+ </tile>
+ <tile id="487" type="tree_stomp"/>
+ <tile id="488">
+  <properties>
+   <property name="parent_id" type="int" value="487"/>
+  </properties>
+ </tile>
+ <tile id="504" type="tree_stomp_short"/>
+ <tile id="505">
+  <properties>
+   <property name="parent_id" type="int" value="504"/>
+  </properties>
+ </tile>
+ <tile id="506" type="bush"/>
+ <tile id="507">
+  <properties>
+   <property name="parent_id" type="int" value="506"/>
+  </properties>
+ </tile>
+ <tile id="508">
+  <animation>
+   <frame tileid="508" duration="100"/>
+   <frame tileid="510" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="509">
+  <animation>
+   <frame tileid="509" duration="100"/>
+   <frame tileid="511" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="517">
+  <properties>
+   <property name="parent_id" type="int" value="485"/>
+  </properties>
+ </tile>
+ <tile id="518">
+  <properties>
+   <property name="parent_id" type="int" value="485"/>
+  </properties>
+ </tile>
+ <tile id="519">
+  <properties>
+   <property name="parent_id" type="int" value="487"/>
+  </properties>
+ </tile>
+ <tile id="520">
+  <properties>
+   <property name="parent_id" type="int" value="487"/>
+  </properties>
+ </tile>
+ <tile id="536">
+  <properties>
+   <property name="parent_id" type="int" value="504"/>
+  </properties>
+ </tile>
+ <tile id="537">
+  <properties>
+   <property name="parent_id" type="int" value="504"/>
+  </properties>
+ </tile>
+ <tile id="538">
+  <properties>
+   <property name="parent_id" type="int" value="506"/>
+  </properties>
+ </tile>
+ <tile id="539">
+  <properties>
+   <property name="parent_id" type="int" value="506"/>
+  </properties>
+ </tile>
+ <tile id="549">
+  <properties>
+   <property name="parent_id" type="int" value="485"/>
+  </properties>
+ </tile>
+ <tile id="550">
+  <properties>
+   <property name="parent_id" type="int" value="485"/>
+  </properties>
+ </tile>
+ <tile id="576" type="grass_plain_obstacle_horizontal"/>
+ <tile id="577">
+  <properties>
+   <property name="parent_id" type="int" value="576"/>
+  </properties>
+ </tile>
+ <tile id="578">
+  <properties>
+   <property name="parent_id" type="int" value="576"/>
+  </properties>
+ </tile>
+ <tile id="579">
+  <animation>
+   <frame tileid="579" duration="100"/>
+   <frame tileid="580" duration="100"/>
+   <frame tileid="581" duration="100"/>
+   <frame tileid="582" duration="100"/>
+   <frame tileid="583" duration="100"/>
+   <frame tileid="584" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="585">
+  <animation>
+   <frame tileid="585" duration="1000"/>
+   <frame tileid="587" duration="1000"/>
+  </animation>
+ </tile>
+ <tile id="586">
+  <animation>
+   <frame tileid="586" duration="1000"/>
+   <frame tileid="588" duration="1000"/>
+  </animation>
+ </tile>
+ <tile id="589" type="log_long"/>
+ <tile id="590">
+  <properties>
+   <property name="parent_id" type="int" value="589"/>
+  </properties>
+ </tile>
+ <tile id="591">
+  <properties>
+   <property name="parent_id" type="int" value="589"/>
+  </properties>
+ </tile>
+ <tile id="596" type="log_short"/>
+ <tile id="597">
+  <properties>
+   <property name="parent_id" type="int" value="596"/>
+  </properties>
+ </tile>
+ <tile id="600" type="log_short_flower"/>
+ <tile id="601">
+  <properties>
+   <property name="parent_id" type="int" value="600"/>
+  </properties>
+ </tile>
+</tileset>
