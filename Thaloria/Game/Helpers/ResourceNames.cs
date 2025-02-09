e7a5b91864928647c505549a81243a858e85b113
DutchJavaDev <borismulder@outlook.com>
2025-02-09 17:32:08 +0100

    Update ResourceNames.cs

--- Thaloria/Game/Helpers/ResourceNames.cs
+++ Thaloria/Game/Helpers/ResourceNames.cs
@@ -3,7 +3,6 @@
   public static class ResourceNames
   {
     public static readonly string ImmortalFont = "immortal_font";
-    public static readonly string TileTexture = "map_tile_set";
     public static readonly string CharaterTileSet = "characters_tile_set";
   }
 }
